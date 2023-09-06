8428a180edce077e8cad6157e12a91a952f1cd23
Andrew <109985553+vorofikin@users.noreply.github.com>
2023-09-06 14:20:02 +0300

    Update lesson2_task06.erl

--- lesson2_task06.erl
+++ lesson2_task06.erl
@@ -1,10 +1,9 @@
 -module(lesson2_task06).
+
 -export([is_palindrome/1]).
 
 is_palindrome(List) -> is_palindrome(List, List, []).
-
 is_palindrome([], List, Acc) ->
   Acc == List;
-
 is_palindrome([H | T], List, Acc) ->
   is_palindrome(T, List, [H | Acc]).
