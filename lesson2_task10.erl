03bfbfd1828837e8293df06f0cfd2689f6f67fc6
Andrew <109985553+vorofikin@users.noreply.github.com>
2023-09-06 14:21:04 +0300

    Update lesson2_task10.erl

--- lesson2_task10.erl
+++ lesson2_task10.erl
@@ -1,17 +1,16 @@
 -module(lesson2_task10).
+
 -import(lesson2_task05, [reverse/1]).
+
 -export([encode/1]).
 
-encode(List) -> encode(List, []).
-
-encode([], Acc) -> reverse(Acc);
-
+encode(List) -> 
+  encode(List, []).
+encode([], Acc) -> 
+  reverse(Acc);
 encode([H | T], []) ->
   encode(T, [{1, H}]);
-
 encode([H | T], [{Counter, H} | Y]) ->
   encode(T, [{Counter + 1, H} | Y]);
-
 encode([H | T], Acc) ->
   encode(T, [{1, H} | Acc]).
-
