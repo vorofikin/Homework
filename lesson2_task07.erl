e239ae20529ab416ed8b5c491177bbde9b39eb92
Andrew <109985553+vorofikin@users.noreply.github.com>
2023-09-06 14:14:36 +0300

    Update lesson2_task07.erl

--- lesson2_task07.erl
+++ lesson2_task07.erl
@@ -1,21 +1,10 @@
 -module(lesson2_task07).
+
 -import(lesson2_task05, [reverse/1]).
+
 -export([flatten/1]).
 
-flatten(List) -> 
-    flatten(List, []).
-
-flatten([], Acc) ->
-    reverse(Acc);
-
-flatten([[] | T], Acc) ->
-    flatten(T, Acc);
-
-flatten([[H | Y] | T], Acc) ->
-    flatten(T, flatten(Y, [H | Acc]));
-
-flatten([H | []], Acc) ->
-    [H | Acc];
-
-flatten([H | T], Acc) ->
-    flatten(T, [H | Acc]).
+flatten(List) -> flatten(List, []).
+flatten([], Acc) -> Acc;
+flatten([H | T], Acc) ->  flatten(H, flatten(T,Acc));
+flatten(H, Acc) ->  [H | Acc].
